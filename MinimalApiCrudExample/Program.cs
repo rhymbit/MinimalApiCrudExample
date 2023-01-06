69ae204aeb56c19da89d7e026792a1b553cd19be
prateek332 <prateekn332@gmail.com>
2023-01-06 21:32:54 +0530

    Removed WeatherForecast record

--- MinimalApiCrudExample/Program.cs
+++ MinimalApiCrudExample/Program.cs
@@ -15,8 +15,3 @@ app.UseHttpsRedirection();
 app.MapUserEndponts();
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
